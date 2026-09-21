38286b361c2e606e4d1cd98820b71f9c5b83f4bf
Aditya-Holkar <123243229+Aditya-Holkar@users.noreply.github.com>
2026-09-21 20:00:23 +0530

    Restore client component directive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useRef, useState } from "react";
 import XauusdDecisionPanel from "./xauusd-decision-panel";
 import { LanguageProvider, languages, useLanguage, type Language } from "./language-context";
